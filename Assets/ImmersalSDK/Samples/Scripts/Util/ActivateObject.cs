ef03db6eed2dcc743a9c4018e4376ef50c99dc27
hitmoose007 <moosathebutt@gmail.com>
2024-02-21 01:27:48 +0500

    Bug finally fixed where firestore shit didnt load hopefully
    Added firebase check to check if firebase had initialized properly

--- Assets/ImmersalSDK/Samples/Scripts/Util/ActivateObject.cs
+++ Assets/ImmersalSDK/Samples/Scripts/Util/ActivateObject.cs
@@ -1,12 +1,68 @@
 using UnityEngine;
+using Firebase;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public class ActivateObject : MonoBehaviour
 {
     public GameObject ContentUI;
     public GameObject NavUI;
     public GameObject WaypointUI;
+    private Queue<System.Action> actionsToRunOnMainThread = new Queue<System.Action>();
 
     public bool activateForEditor = false;
+
+    void Start()
+    {
+        FirebaseApp
+            .CheckAndFixDependenciesAsync()
+            .ContinueWith(task =>
+            {
+                if (task.IsCompleted)
+                {
+                    Firebase.DependencyStatus dependencyStatus = task.Result;
+                    if (dependencyStatus == Firebase.DependencyStatus.Available)
+                    {
+                        // Firebase is ready for use, enqueue any actions that need to run on the main thread
+                        EnqueueActionOnMainThread(() =>
+                        {
+                            if (activateForEditor)
+                            {
+                                ActivateContentUI();
+                                ActivateNavUI();
+                                ActivateWaypointUI();
+                                // Optionally activate other UI elements as needed
+                            }
+                        });
+                    }
+                    else
+                    {
+                        Debug.LogError(
+                            $"Could not resolve all Firebase dependencies: {dependencyStatus}"
+                        );
+                        // Handle the case where Firebase is not available
+                    }
+                }
+            });
+    }
+
+    void Update()
+    {
+        // Execute all actions queued to run on the main thread
+        while (actionsToRunOnMainThread.Count > 0)
+        {
+            actionsToRunOnMainThread.Dequeue().Invoke();
+        }
+    }
+
+    // Enqueues an action to be performed on the main thread
+    private void EnqueueActionOnMainThread(System.Action action)
+    {
+        lock (actionsToRunOnMainThread)
+        {
+            actionsToRunOnMainThread.Enqueue(action);
+        }
+    }
 
     public void ActivateContentUI()
     {
@@ -21,16 +77,5 @@ public class ActivateObject : MonoBehaviour
     public void ActivateWaypointUI()
     {
         WaypointUI.SetActive(true);
-    }
-
-    //start
-    public void Start()
-    {
-        if (activateForEditor)
-        {
-            ContentUI.SetActive(true);
-            NavUI.SetActive(true);
-            WaypointUI.SetActive(true);
-        }
     }
 }
