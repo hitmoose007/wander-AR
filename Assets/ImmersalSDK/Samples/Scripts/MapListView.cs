825947927df9b302fc8e3357246b962f935bae12
hitmoose007 <moosathebutt@gmail.com>
2024-02-28 14:52:56 +0500

    changed maps to map and remove struct
    yeh removed struct

--- Assets/ImmersalSDK/Samples/Scripts/MapListView.cs
+++ Assets/ImmersalSDK/Samples/Scripts/MapListView.cs
@@ -19,28 +19,41 @@ public class MapListView : MonoBehaviour
     void Start()
     {
         db = FirebaseFirestore.DefaultInstance;
-        Query allMapsQuery = db.Collection("maps");
+        Query allMapsQuery = db.Collection("map");
 
-        allMapsQuery.GetSnapshotAsync().ContinueWithOnMainThread(task =>
-        {
-            QuerySnapshot allMapsQuerySnapshot = task.Result;
-
-            foreach(DocumentSnapshot documentSnapshot in allMapsQuerySnapshot.Documents)
+        allMapsQuery
+            .GetSnapshotAsync()
+            .ContinueWithOnMainThread(task =>
             {
-                Debug.Log(String.Format("Document data for {0} document:", documentSnapshot.Id));
+                QuerySnapshot allMapsQuerySnapshot = task.Result;
 
-                Maps map = documentSnapshot.ConvertTo<Maps>();
-                GameObject item = Instantiate(listItemPrefab, listItemHolder);
-                item.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = map.mapName.ToString();
+                foreach (DocumentSnapshot documentSnapshot in allMapsQuerySnapshot.Documents)
+                {
+                    Debug.Log(
+                        String.Format("Document data for {0} document:", documentSnapshot.Id)
+                    );
 
-                // Dictionary<string, object> map = documentSnapshot.ToDictionary();
-                // foreach (KeyValuePair<string, object> pair in map)
-                // {
-                //     Debug.Log(String.Format("{0}: {1}", pair.Key, pair.Value));
-                // }
-                // // Newline to separate entries
-                // Debug.Log(" ");
-            }   
-        });
+                    Dictionary<string, object> mapData = documentSnapshot.ToDictionary();
+                    GameObject item = Instantiate(listItemPrefab, listItemHolder);
+
+                    if (mapData.ContainsKey("name") == false)
+                    {
+                        Debug.LogWarning("Map name");
+                        continue;
+                    }
+                    //check if key exists
+                    item.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = mapData[
+                        "name"
+                    ].ToString();
+
+                    // Dictionary<string, object> map = documentSnapshot.ToDictionary();
+                    // foreach (KeyValuePair<string, object> pair in map)
+                    // {
+                    //     Debug.Log(String.Format("{0}: {1}", pair.Key, pair.Value));
+                    // }
+                    // // Newline to separate entries
+                    // Debug.Log(" ");
+                }
+            });
     }
 }
