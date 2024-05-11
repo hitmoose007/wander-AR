999bec64fc4eb50ac5c3b43b8075cf5578e4b91c
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2024-05-11 17:47:49 +0500

    Revert "bug fix"
    This reverts commit 180b1038facc472cd5388c70020a26306107a736.

--- Assets/ImmersalSDK/Samples/Scripts/Navigation/TargetName.cs
+++ Assets/ImmersalSDK/Samples/Scripts/Navigation/TargetName.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using TMPro;
 
-namespace TargetName
+namespace TargetNamespace
 {
     // Import the namespace where IsNavigationTarget is defined
     using Immersal.Samples.Navigation;
