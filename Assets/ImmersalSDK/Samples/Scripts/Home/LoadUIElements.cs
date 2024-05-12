e76e6a977c2723a772d1f2331f8854c1ca3be74a
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2024-05-12 15:43:10 +0500

    Completed User Info panel on Home Screen

--- Assets/ImmersalSDK/Samples/Scripts/Home/LoadUIElements.cs
+++ Assets/ImmersalSDK/Samples/Scripts/Home/LoadUIElements.cs
@@ -12,10 +12,6 @@ public class LoadUIElements : MonoBehaviour
     public GameObject cameraPanel;
     public Button headerTabButton;
 
-    private void Awake()
-    {
-        
-    }
     // Start is called before the first frame update
     void Start()
     {
